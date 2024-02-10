66c865245c523a8c8e67358b37e797aa6602151d
mariam <mariamelmogy3@gmail.com>
2024-02-10 20:45:47 +0200

    - create Text Styles class - add styles text

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class ResponsiveDashBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
       home: DashBoardView(),
     );
   }
