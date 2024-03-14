ce8f8bace4492537d8f1a1142eb1e5d30a43a9c7
mariam <mariamelmogy3@gmail.com>
2024-03-14 11:40:27 +0200

    - refactor the code of income section

--- lib/views/widgets/income_section.dart
+++ lib/views/widgets/income_section.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/widgets/custom_background_container.dart';
-import 'package:responsive_dash_board/views/widgets/income_chart.dart';
-import 'package:responsive_dash_board/views/widgets/income_details.dart';
+import 'income_section_body.dart';
 import 'income_section_header.dart';
 
 class IncomeSection extends StatelessWidget {
@@ -13,13 +12,7 @@ class IncomeSection extends StatelessWidget {
       child: Column(
         children: [
           IncomeSectionHeader(),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(child: IncomeChart()),
-              Expanded(child: IncomeDetails()),
-            ],
-          ),
+          IncomSectionBody(),
         ],
       ),
     );
