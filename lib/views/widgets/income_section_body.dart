221ca6269fa8964fb41d19fabafcaee447e64204
mariam <mariamelmogy3@gmail.com>
2024-03-14 12:09:16 +0200

    - refactor code of income chart - create new detailed income chart widget

--- lib/views/widgets/income_section_body.dart
+++ lib/views/widgets/income_section_body.dart
@@ -3,6 +3,7 @@ import 'package:responsive_dash_board/views/widgets/income_chart.dart';
 import 'package:responsive_dash_board/views/widgets/income_details.dart';
 
 import '../../utils/size_config.dart';
+import 'detailed_income_chart.dart';
 
 class IncomSectionBody extends StatelessWidget {
   const IncomSectionBody({
@@ -13,7 +14,12 @@ class IncomSectionBody extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
     return width >= SizeConfig.desktop && width < 1750
-        ? const SizedBox()
+        ? const Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: DetailedIncomeChart(),
+            ),
+          )
         : const Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
